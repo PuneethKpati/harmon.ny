2b511dba22b3d09f4d06fc26efb9b6cdb85f264b
puneeth.kpati <puneeth.kpati@gmail.com>
2020-12-27 22:14:27 +1100

    added logo screen and basic login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
+        {console.log(window.location.origin)}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You must <code>login</code> with your Spotify Account...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://accounts.spotify.com/authorize?response_type=code&client_id=c040d3bbb7854c27a1e4ca23de239e5e&redirect_uri=http://localhost:3000/"
+          target="_top"
           rel="noopener noreferrer"
         >
-          Learn React
+          Spotify Login
         </a>
       </header>
     </div>
