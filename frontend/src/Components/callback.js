f07e28555b16b0d64bc743406b861f51e06fc518
puneeth.kpati <puneeth.kpati@gmail.com>
2021-01-05 22:31:09 +1100

    background iumage and functionalities added

--- frontend/src/Components/callback.js
+++ frontend/src/Components/callback.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import qs from 'qs';
+
 const Callback = (props) => {
     
-    /* const [resp, setResp] = useState(''); */
+    const [resp, setResp] = useState('');
     const [code, setCode] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).code); 
+    const [error, setError] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).error);
 
     useEffect(() => {
-        
         if (code !== undefined) {
             axios
-            .post('http://localhost:5050/login/access_token', {'redirect_uri':'http://localhost:3000/callback', 'code':code, 'grant_type':'authorization_code'})
+            .post('http://localhost:5050/authorization/access_token', {'redirect_uri':'http://localhost:3000/callback', 'code':code, 'grant_type':'authorization_code'})
             .then((res) => {
             console.log(res.data)
+            setResp(res.data)
             })
         }
         
@@ -20,7 +23,11 @@ const Callback = (props) => {
 
     return (
         <div>
-            callback
+            {
+                error !== undefined && 
+                <Redirect to='/' />
+            }
+            {resp['error']}
         </div>
     );
 }
--- frontend/src/Components/login.js
+++ frontend/src/Components/login.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import logo from '../res/logo2.png';
+import background from '../res/Background.png';
+import { makeStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import LockOpenRoundedIcon from '@material-ui/icons/LockOpenRounded';;
 
+const useStyles = makeStyles(() => ({
+    back: {
+        backgroundImage : `url(${background})`,
+        position: 'fixed',
+        width: '100%',
+        height:'100%',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      },
+}));
 
 const Login = (props) => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <header className="App-header">
+        <div className={ classes.back } >
+            <header >
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                You must <code>login</code> with your Spotify Account...
-                </p>
-                <a
-                href="https://accounts.spotify.com/authorize?response_type=code&client_id=c040d3bbb7854c27a1e4ca23de239e5e&redirect_uri=http://localhost:3000/callback"
+                <Button
+                endIcon={<LockOpenRoundedIcon />}
+                variant="contained"
                 target="_top"
-                rel="noopener noreferrer"
+                colour="#086213"
+                href="https://accounts.spotify.com/authorize?response_type=code&client_id=c040d3bbb7854c27a1e4ca23de239e5e&redirect_uri=http://localhost:3000/callback"
                 >
-                Spotify Logins
-                </a>
+                Login
+                </Button>
             </header>
             
         </div>
