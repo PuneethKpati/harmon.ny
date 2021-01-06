1bf60330fd176f76171591cbce971dd628f06916
puneeth.kpati <puneeth.kpati@gmail.com>
2021-01-06 21:53:36 +1100

    new home and logo added for the main welcome page

---
+++ frontend/src/Components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bar from './bar';
+
+const Home = (props) => {
+
+    return (
+        <div className="Root" >
+            <Bar />
+        </div>
+    );
+}
+
+export default Home;
