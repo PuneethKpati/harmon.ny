a4fda6751706c71e545650eb51a395643c8e40b5
puneeth.kpati <puneeth.kpati@gmail.com>
2021-01-07 17:16:29 +1100

    added the welcome body

--- frontend/src/Components/home.js
+++ frontend/src/Components/home.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import Bar from './bar';
+import { makeStyles, withTheme } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { LoginButton } from './login';
+
+const useStyles = makeStyles((theme) => ({
+    bodyContainer: {
+        position: 'relative',
+        height: '50%',
+        opacity: '70%',
+    },
+    homeBody: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        maxWidth: '40%'
+    },
+    title: {
+        color: 'white',
+        paddingBottom: '15px'
+    },
+    description: {
+        color: 'white',
+        fontSize: '100%',
+        paddingBottom: '30px'
+    }
+}));
 
 const Home = (props) => {
+    const classes = useStyles();
 
     return (
         <div className="Root" >
             <Bar />
+            <div className={ classes.bodyContainer }>
+                <div className={ classes.homeBody }>
+                    <Typography variant='h1' className={ classes.title }>
+                        HARMON.NY
+                    </Typography>
+                    <Typography variant='body1' className={ classes.description }>
+                        Your Friends know you well, maybe too well... <br/>Surely not everything about you, but just enough? 
+                        I'm sure you like music and I'm sure your friends know that about you. 
+                        God Damnit! Just get them to do this quiz about you, I'm done with the Exposition.
+                    </Typography>
+                    <LoginButton /> 
+                </div>
+                
+            </div>
         </div>
     );
 }
--- frontend/src/Components/login.js
+++ frontend/src/Components/login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import Button from '@material-ui/core/Button';
 
 const LoginIcon = ({style}) => {
     return (
@@ -18,4 +20,19 @@ const LoginIcon = ({style}) => {
     );
 }
 
+export const LoginButton = () => {
+    return (
+        <div>
+            <Button 
+            variant='contained' 
+            style={{color:'white', backgroundColor: '#086213', borderCollapse: '#086213', fontSize: '100%', fontStyle:'bold'}}
+            href='https://accounts.spotify.com/authorize?response_type=code&client_id=c040d3bbb7854c27a1e4ca23de239e5e&redirect_uri=http://localhost:3000/callback'>
+                Login With Spotify 
+                <LockOpenIcon />
+            </Button>
+        </div>
+    )
+};
+
 export default LoginIcon;
+
