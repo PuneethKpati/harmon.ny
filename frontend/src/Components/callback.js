faf2f7726fe7f09d1dba00074d35f8297e683cb5
puneeth.kpati <puneeth.kpati@gmail.com>
2021-01-07 16:12:02 +1100

    added proper axios functionality

--- frontend/src/Components/callback.js
+++ frontend/src/Components/callback.js
@@ -3,8 +3,9 @@ import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import qs from 'qs';
 import Bar from './bar';
+
 const Callback = (props) => {
-    
+    const [resp, setResp] = useState([])
     const [code, setCode] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).code); 
     const [error, setError] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).error);
 
@@ -13,11 +14,13 @@ const Callback = (props) => {
             axios
             .post('http://localhost:5050/authorization/access_token', {'redirect_uri':'http://localhost:3000/callback', 'code':code, 'grant_type':'authorization_code'})
             .then((res) => {
-            console.log(res.data)
+                console.log(res.data)
+                setResp(res.data)
+                console.log(resp)
             })
         }
         
-    });
+    }, []);
 
     return (
         <div className="Root">
@@ -26,6 +29,10 @@ const Callback = (props) => {
                 error !== undefined && 
                 <Redirect to='/' />
             }
+            <div color='white'>
+            <p >{JSON.stringify(resp)}</p>
+            </div>
+            
         </div>
     );
 }
