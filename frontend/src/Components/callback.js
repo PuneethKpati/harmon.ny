7c88df471329a1448aecf0beaab1fecfe8934b74
puneeth.kpati <puneeth.kpati@gmail.com>
2021-01-07 17:17:11 +1100

    completed proper redirecting fro the call back page

--- frontend/src/Components/callback.js
+++ frontend/src/Components/callback.js
@@ -5,7 +5,7 @@ import qs from 'qs';
 import Bar from './bar';
 
 const Callback = (props) => {
-    const [resp, setResp] = useState([])
+    const [resp, setResp] = useState({})
     const [code, setCode] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).code); 
     const [error, setError] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).error);
 
@@ -16,21 +16,24 @@ const Callback = (props) => {
             .then((res) => {
                 console.log(res.data)
                 setResp(res.data)
-                console.log(resp)
             })
         }
-        
     }, []);
 
     return (
         <div className="Root">
             <Bar />
-            {
+            { 
+                code !== undefined && 
+                <Redirect to='/profile' />
+            }
+            { 
                 error !== undefined && 
                 <Redirect to='/' />
             }
             <div color='white'>
             <p >{JSON.stringify(resp)}</p>
+            
             </div>
             
         </div>
